414541a782f64d2390a660525e41a04f66e5f3eb
camunity <cameron.flowers13@gmail.com>
2015-03-31 16:32:58 -0500

    updated storyboard and segue

--- LostCharactersChallenge/LCCListViewController.m
+++ LostCharactersChallenge/LCCListViewController.m
@@ -135,6 +135,8 @@
 
 }
 
+
+
 -(void)setEditing:(BOOL)editing animated:(BOOL)animated
 {
     self.charactersTableView.allowsMultipleSelectionDuringEditing = editing;
@@ -143,11 +145,23 @@
 
 #pragma mark - Navigation
 
+- (BOOL) shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    if ([identifier isEqualToString:@"DetailSegue"]) {
+        if (self.editing) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-//    // Get the new view controller using [segue destinationViewController].
-//    // Pass the selected object to the new view controller.
-//}
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+
+}
+
+
 
 
 @end
