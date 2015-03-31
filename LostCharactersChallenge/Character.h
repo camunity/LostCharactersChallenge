91e323644730311810bedafbe9a0241643ce1b9b
garciajg <pepe_garcia1606@yahoo.com>
2015-03-31 14:21:44 -0500

    Custom Subclass

---
+++ LostCharactersChallenge/Character.h
@@ -0,0 +1,21 @@
+//
+//  Character.h
+//  LostCharactersChallenge
+//
+//  Created by Jose Garcia on 3/31/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Character : NSManagedObject
+
+@property (nonatomic, retain) NSString * actor;
+@property (nonatomic, retain) NSNumber * age;
+@property (nonatomic, retain) NSString * gender;
+@property (nonatomic, retain) NSString * haircolor;
+@property (nonatomic, retain) NSString * passenger;
+
+@end
---
+++ LostCharactersChallenge/Character.m
@@ -0,0 +1,20 @@
+//
+//  Character.m
+//  LostCharactersChallenge
+//
+//  Created by Jose Garcia on 3/31/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import "Character.h"
+
+
+@implementation Character
+
+@dynamic actor;
+@dynamic age;
+@dynamic gender;
+@dynamic haircolor;
+@dynamic passenger;
+
+@end
