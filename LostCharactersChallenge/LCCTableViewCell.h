e7ff725f94bfb3e8a030ef822924bf8d7dc09106
garciajg <pepe_garcia1606@yahoo.com>
2015-03-31 14:38:13 -0500

    custom cell class

---
+++ LostCharactersChallenge/LCCTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LCCTableViewCell.h
+//  LostCharactersChallenge
+//
+//  Created by Jose Garcia on 3/31/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCCTableViewCell : UITableViewCell
+
+@end
---
+++ LostCharactersChallenge/LCCTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  LCCTableViewCell.m
+//  LostCharactersChallenge
+//
+//  Created by Jose Garcia on 3/31/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import "LCCTableViewCell.h"
+
+@implementation LCCTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
